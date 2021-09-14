7859b9f87b079b49f8cc77cfe9b78d759f247aaf
gourish13 <62661395+gourish13@users.noreply.github.com>
2021-09-14 21:30:28 +0530

    deleted missing.services.js Redundant with creater.services.js

--- app/components/missing/missing.services.js
+++
@@ -1,8 +0,0 @@
-angular.module('missing')
-.service('particleJS', function() {
-	this.startParticleMotion = function() {
-		particlesJS.load("particles-js", './statics/particles.conf.json', function() {
-			console.log('Particle.js config loaded on Missing Page');
-		});
-	}
-});
